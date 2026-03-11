5efc79c00450480309779e7d5f1dbd1163d233e4
don <donny77776@gmail.com>
2026-03-11 11:47:11 +0000

    added home page

---
+++ Scannit/Scannit/Pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, ActivityIndicator, Image } from 'react-native';
+
+export default function Home() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
