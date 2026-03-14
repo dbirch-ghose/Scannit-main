7237f4f10b3f3cde7949be1d7ffeaa495f8373ca
don <donny77776@gmail.com>
2026-03-14 15:23:09 +0000

    added bottom bar

--- Scannit/Scannit/Pages/Home.js
+++ Scannit/Scannit/Pages/Home.js
@@ -1,26 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, ActivityIndicator, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function Home() {
+export default function App() {
 
+const MyTabs = createBottomTabNavigator({
+  screens: {
+    Home: HomeScreen,
+    Profile: ProfileScreen,
+  },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 }
